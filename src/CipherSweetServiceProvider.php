2f7f283418c555bfdf1d073ac6a57d71cd8f2af5
Michael Cordingley <Michael.Cordingley@gmail.com>
2019-03-11 15:12:47 -0400

    Define Laravel service provider.

---
+++ src/CipherSweetServiceProvider.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace ParagonIE\EloquentCipherSweet;
+
+use Illuminate\Support\ServiceProvider;
+use ParagonIE\CipherSweet\Backend\FIPSCrypto;
+use ParagonIE\CipherSweet\Backend\ModernCrypto;
+use ParagonIE\CipherSweet\CipherSweet;
+use ParagonIE\CipherSweet\Contract\BackendInterface;
+use ParagonIE\CipherSweet\Contract\KeyProviderInterface;
+use ParagonIE\CipherSweet\Exception\CryptoOperationException;
+use ParagonIE\CipherSweet\KeyProvider\FileProvider;
+use ParagonIE\CipherSweet\KeyProvider\RandomProvider;
+use ParagonIE\CipherSweet\KeyProvider\StringProvider;
+
+class CipherSweetServiceProvider extends ServiceProvider
+{
+    /**
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/config/ciphersweet.php' => config_path('ciphersweet.php'),
+        ]);
+    }
+
+    /**
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(CipherSweet::class, function () {
+            return new CipherSweet($this->buildKeyProvider($this->buildBackend()));
+        });
+    }
+
+    /**
+     * @return BackendInterface
+     */
+    protected function buildBackend(): BackendInterface
+    {
+        switch (config('ciphersweet.backend')) {
+            case 'fips':
+                return new FIPSCrypto;
+            case 'nacl':
+            default:
+                return new ModernCrypto;
+        }
+    }
+
+    /**
+     * @param BackendInterface $backend
+     * @return KeyProviderInterface
+     * @throws CryptoOperationException
+     */
+    protected function buildKeyProvider(BackendInterface $backend): KeyProviderInterface
+    {
+        switch (config('ciphersweet.provider')) {
+            case 'file':
+                return new FileProvider($backend, config('ciphersweet.file.path'));
+            case 'string':
+                return new StringProvider($backend, config('ciphersweet.string.key'));
+            case 'random':
+            default:
+                return new RandomProvider($backend);
+        }
+    }
+}
